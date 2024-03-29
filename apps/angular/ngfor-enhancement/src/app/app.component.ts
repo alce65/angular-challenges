69d1f5763ddcef54f2f520a20a53a028a0297b6b
alce65 <alce65@hotmail.es>
2024-03-29 20:32:07 +0100

    feat: solve challenge (only new control flow)

--- apps/angular/ngfor-enhancement/src/app/app.component.ts
+++ apps/angular/ngfor-enhancement/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { NgFor, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { timer } from 'rxjs';
 
 interface Person {
   name: string;
@@ -8,7 +14,7 @@ interface Person {
 @Component({
   standalone: true,
   imports: [NgFor, NgIf],
-  selector: 'app-root',
+  selector: 'app-root-old',
   template: `
     <ng-container *ngIf="persons.length > 0; else emptyList">
       <div *ngFor="let person of persons">
@@ -20,6 +26,33 @@ interface Person {
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppOldComponent {
   persons: Person[] = [];
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    @if (persons().length) {
+      @for (person of persons(); track person.name) {
+        <p>{{ person.name }}</p>
+      }
+    } @else {
+      <p>The list is empty !!</p>
+    }
+  `,
+  styles: [],
+})
+export class AppComponent implements OnInit {
+  persons = signal<Person[]>([]);
+
+  ngOnInit(): void {
+    timer(2000).subscribe(() => {
+      this.persons.update(() => [{ name: 'Pepe' }, { name: 'Ernestina' }]);
+      console.log('timer', this.persons());
+    });
+  }
 }
