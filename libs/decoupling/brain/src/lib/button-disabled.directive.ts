3cb05f642cc936b8eede28d4e72ec52cfc67ff14
alce65 <alce65@hotmail.es>
2024-03-30 18:47:18 +0100

    feat: solve challenge

--- libs/decoupling/brain/src/lib/button-disabled.directive.ts
+++ libs/decoupling/brain/src/lib/button-disabled.directive.ts
@@ -1,12 +1,26 @@
 /* eslint-disable @angular-eslint/directive-selector */
 /* eslint-disable @angular-eslint/no-host-metadata-property */
-import { Directive, WritableSignal, signal } from '@angular/core';
+import {
+  BUTTON_STATE_TOKEN,
+  ButtonState,
+} from '@angular-challenges/decoupling/core';
+import { Directive, WritableSignal, forwardRef, signal } from '@angular/core';
 
-export type ButtonState = 'enabled' | 'disabled';
+// Gracias al token BUTTON_STATE_TOKEN,
+// BtnDisabledDirective puede ser independiente de BtnHelmetDirective
+
+// BtnDisabledDirective actúa como provider de una instancia de ella misma
+// cuando un inyector solicita BUTTON_STATE_TOKEN,
 
 @Directive({
   selector: 'button[btnDisabled]',
   standalone: true,
+  providers: [
+    {
+      provide: BUTTON_STATE_TOKEN,
+      useExisting: forwardRef(() => BtnDisabledDirective),
+    },
+  ],
   host: {
     '(click)': 'toggleState()',
   },
